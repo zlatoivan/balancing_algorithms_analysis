c18ad2ef81daaa44292832e32015ae5aa6268a7a
zlatoivan <zlatoivan4@gmail.com>
2024-05-27 00:49:27 +0300

    small fix

--- balancer/internal/server/balancer/random.go
+++ balancer/internal/server/balancer/random.go
@@ -9,6 +9,6 @@ type Random struct{}
 
 func (b Random) Balance() string {
 	//backend := "https://localhost:7071"
-	backend := fmt.Sprintf("https://%d.zlatoivan.ru", 1+rand.IntN(1))
+	backend := fmt.Sprintf("https://%d.zlatoivan.ru", 3+rand.IntN(1))
 	return backend
 }
