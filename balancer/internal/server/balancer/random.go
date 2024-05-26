6265d5efd07db2228b6d84d81d67b6bb92b48480
zlatoivan <zlatoivan4@gmail.com>
2024-05-26 21:52:34 +0300

    small fix

--- balancer/internal/server/balancer/random.go
+++ balancer/internal/server/balancer/random.go
@@ -8,7 +8,7 @@ import (
 type Random struct{}
 
 func (b Random) Balance() string {
-	backend := fmt.Sprintf("%d.zlatoivan.ru", 1+rand.IntN(2))
+	backend := fmt.Sprintf("%d.zlatoivan.ru", 1+rand.IntN(0))
 	fmt.Println("[random] balancer have chosen backend: ", backend)
 	return backend
 }
