ed8a5bd51b7fb9f5454af7443c78c84eaa0268fa
zlatoivan <zlatoivan4@gmail.com>
2024-05-27 01:56:15 +0300

    small fix

--- load_backend/internal/server/mul_matrix.go
+++ load_backend/internal/server/mul_matrix.go
@@ -41,6 +41,7 @@ func green(s string) string {
 func (s Server) MulMatrices(_ http.ResponseWriter, _ *http.Request) {
 	start := time.Now()
 	n := s.matrixSize
+	fmt.Println(n)
 	matrixA := genRandMatrix(n)
 	matrixB := genRandMatrix(n)
 	MultiplyMatrix(n, matrixA, matrixB)
