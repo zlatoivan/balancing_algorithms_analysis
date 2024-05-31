8d29fc9912e05275aa6121dd1d372742bd71c4f1
zlatoivan <zlatoivan4@gmail.com>
2024-05-31 15:56:05 +0300

    small fix

--- balancer/internal/server/balancer/round_robin.go
+++ balancer/internal/server/balancer/round_robin.go
@@ -12,10 +12,15 @@ type RoundRobin struct {
 }
 
 func (b *RoundRobin) Balance() string {
+	b.mx.RLock()
 	backend := b.Hosts[b.Last]
+	b.mx.RUnlock()
+
 	fmt.Println(b.Last, "  ", (b.Last+1)%len(b.Hosts))
+
 	b.mx.Lock()
 	b.Last = (b.Last + 1) % len(b.Hosts)
-	b.mx.Lock()
+	b.mx.Unlock()
+
 	return backend
 }
