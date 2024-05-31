75143f4d46d8ac58497196cc6ae39b2f22228908
zlatoivan <zlatoivan4@gmail.com>
2024-05-31 15:54:54 +0300

    small fix

--- balancer/internal/server/balancer/round_robin.go
+++ balancer/internal/server/balancer/round_robin.go
@@ -1,15 +1,21 @@
 package balancer
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 type RoundRobin struct {
 	Hosts []string
 	Last  int
+	mx    sync.RWMutex
 }
 
 func (b *RoundRobin) Balance() string {
 	backend := b.Hosts[b.Last]
 	fmt.Println(b.Last, "  ", (b.Last+1)%len(b.Hosts))
+	b.mx.Lock()
 	b.Last = (b.Last + 1) % len(b.Hosts)
+	b.mx.Lock()
 	return backend
 }
