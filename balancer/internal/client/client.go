e05053d20e9a091795e58706e7a7bbf4598a522f
zlatoivan <zlatoivan4@gmail.com>
2024-06-03 02:12:46 +0300

    small fix

--- balancer/internal/client/client.go
+++ balancer/internal/client/client.go
@@ -42,11 +42,9 @@ func main() {
 	start := time.Now()
 	n := 10
 
-	for i := 0; i < n; i++ {
-		ping()
-	}
+	//sequentially(n)
 
-	//parallel(n)
+	parallel(n)
 
 	sec := time.Since(start).Seconds()
 	rps := float64(n) / sec
